b89207d3a0aa9212e100d7972b03a7000f8040cc
chetanbhoyar22 <cab9722@gmail.com>
2021-02-14 20:03:19 +0530

    REACT.JS-DataBinding

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState} from 'react';
+import './Chetan.css';
 function App() {
+  
+  const [myCity, ChangeMyCity] = useState("Pune");
+
+  const changeInputValue = (event) =>
+  {
+    ChangeMyCity(event.target.value);
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main_body">
+      <h1>{myCity}</h1>
+      <b>1.Text</b> <input 
+      type="text" 
+      onChange={changeInputValue}
+      />
+      <br/><br/>
+      <b>2.Number</b> <input 
+      type="number"
+      onChange={changeInputValue}
+      />
+      <br/><br/>
+      <b>3.Email</b> <input 
+      type="email"
+      onChange={changeInputValue} 
+      />
+      <br/><br/>
+      <b>4.Password</b> <input 
+      type="password" 
+      />
+      <br/><br/>
+      <b>5.Date</b> <input 
+      type="date" 
+      min="2021-03-01"
+      max="2021-04-30"
+      onChange={changeInputValue}
+      />
+      <br/><br/>
+      <b>6.Time</b> <input 
+      type="time" 
+      onChange={changeInputValue}
+      />
+      <br/><br/>
+      <b>7.Checkbox</b> <input 
+      type="checkbox" 
+      value='10th'
+      onChange={changeInputValue}
+      />10th
+       <input 
+      type="checkbox" 
+      value='12th'
+      onChange={changeInputValue}
+      />12th
+       <input 
+      type="checkbox" 
+      value='Gradution'
+      onChange={changeInputValue}
+      />Gradution
+      <br/><br/>
+      <b>8.Radio</b>
+      <input 
+      type="radio" 
+      value="Male"
+      name="Gender"
+      onChange={changeInputValue}
+      />Male
+      <input 
+      type="radio" 
+      value="Female"
+      name="Gender"
+      onChange={changeInputValue}
+      />Female
+      <br/><br/>
+      <b>9.Select </b>
+      <b>City : </b>
+      <select onChange={changeInputValue}>
+        <option>Pune</option>
+        <option>Mumbai</option>
+        <option>Delhi</option>
+        <option>Aurangabad</option>
+        <option>Amravati</option>
+        <option>Nagpur</option>
+        <option>Goa</option>
+        
+      </select>
+
+      
     </div>
   );
 }
